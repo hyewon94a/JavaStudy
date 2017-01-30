1a5ff1dd4901f7f00a50968cf85962f8f18be013
hyewon94a <hyewon94a@gmail.com>
2017-01-30 21:17:13 +0900

    Add files via upload

---
+++ WhileTest.java
@@ -0,0 +1,12 @@
+public class WhileTest {
+ 
+    public static void main(String[] args) {
+    	int i = 0;
+    	while(i<10){
+    	    System.out.println("Coding Everybody"+i);
+    	    i++;
+    	}
+        }
+ 
+    }
+ 
---
+++ tkdeka.java
@@ -0,0 +1,14 @@
+
+public class tkdeka {
+
+	public static void main(String[] args){
+		
+		String[] studentGroup = { "기린", "사자", "코끼리", "상어"};
+		for (int i = 0; i < studentGroup.length; i++) {
+            String member = studentGroup[i];
+            System.out.println(member + "이 상담을 받았습니다");
+        }
+		System.out.println("상담끝");
+		
+	}
+}
